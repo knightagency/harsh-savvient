e99e9ca256868f2bb5d0b6ece5939dbce6d2393f
aknightdev <developer@knightagency.com.au>
2022-04-05 11:46:02 +0530

    pushed by Knight agency at 05-04-2022-11-46

--- src/components/curve-right2.js
+++ src/components/curve-right2.js
@@ -11,7 +11,7 @@ const CurveRight2 = (props) => {
           <div className="image-warpper">
             <img className="img-fluid" src={props.img?.mediaItemUrl} alt={props.img?.altText} />
           </div>
-          <div className="ins-content2">
+          <div className="ins-content2 ins-content222">
             <h2 className="ins-banner-heading2">{props.title}</h2>
             <div className="ins-banner-details2" dangerouslySetInnerHTML={{ __html: props.text }}></div>
             {props.btn1Link !== null && props.btn1Link !== "" ? <Link className="btn btn-primary me-3 business-button" to={props.btn1Link}>{props.btn1Txt}</Link> : ""}
