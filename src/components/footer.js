83823b67555c47b4c86ac7913e13144b63519a08
aknightdev <developer@knightagency.com.au>
2022-07-27 09:20:30 +0530

    pushed by Knight agency on 27-27-2022-09-20

--- src/components/footer.js
+++ src/components/footer.js
@@ -72,8 +72,19 @@ const links = [
   }
 ]
 
-const FooterMain = ({ siteTitle }) => (
-  <footer>
+const FooterMain = ({ siteTitle }) => {
+  const [showModal, setModal] = React.useState('');
+  React.useEffect(() => {
+     if(window.location.href.includes('thank-you')){
+      setModal('<script type="text/javascript">_linkedin_partner_id = "2334636";window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];window._linkedin_data_partner_ids.push(_linkedin_partner_id);</script><script type="text/javascript">(function(l) {if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};window.lintrk.q=[]}var s = document.getElementsByTagName("script")[0];var b = document.createElement("script");b.type = "text/javascript";b.async = true;b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";s.parentNode.insertBefore(b, s);})(window.lintrk);</script><noscript><img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=2334636&fmt=gif" /></noscript>')
+     } else {
+      setModal('');
+     }
+     return () => {
+       setModal('');
+     }
+   }, [])
+  return (<footer>
     <div className="container">
       <div className="row">
         <div className="col-lg-2 col-md-4 col-xs-12">
@@ -112,7 +123,8 @@ const FooterMain = ({ siteTitle }) => (
         <div className="col-md-4 col-sm-12 text-end legal"><Link to="/privacy-policy">Legal & Privacy</Link></div>
       </div>
     </div>
+    <div dangerouslySetInnerHTML={{__html: showModal }} />
   </footer>
-)
+)}
 
 export default FooterMain
